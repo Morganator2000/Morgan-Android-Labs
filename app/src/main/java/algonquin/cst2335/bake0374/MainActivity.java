6b163277e5bca2f976acb88b30935df77c651299
morga <morganbakelmun@hotmail.com>
2023-05-11 14:00:36 -0400

    Initial commit.

---
+++ app/src/main/java/algonquin/cst2335/bake0374/MainActivity.java
@@ -0,0 +1,14 @@
+package algonquin.cst2335.bake0374;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
