26caa0789dd006ae30cda182b506d4495be7c0dd
morga <morganbakelmun@hotmail.com>
2023-05-24 19:32:53 -0400

    Almost finished lab 2. I need: 1. Why is logo_algonquin.png underlined? 2. Why isn't the text changing when I press the button? 3. Why aren't the coffee buttons changing to match each other? 4. Where is the image button?

--- app/java/MainActivity.java
+++ app/java/MainActivity.java
@@ -10,6 +10,42 @@ public class MainActivity extends AppCompatActivity {
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(variableBinding.getRoot());
 
+        CharSequence text = "The value is now:" + isChecked;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(this, text, duration);
+
+        model.isSelected.observe (this, selected -> {
+            variableBinding.checkBox.setChecked(selected);
+            variableBinding.radioButton.setChecked(selected);
+            variableBinding.switch1.setChecked(selected);
+        });
+
+        radioButton.setOnCheckedChangeListener((btn, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+            textview.setText("Do you drink coffee?" + isChecked);
+            toast.show();
+        } );
+        checkBox.setOnCheckedChangeListener((btn, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+            textview.setText("Do you drink coffee?" + isChecked);
+            toast.show();
+        });
+        switch1.setOnCheckedChangeListener((btn, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+            textview.setText("Do you drink coffee?" + isChecked);
+            toast.show();
+        } );
+
+        variableBinding.myimagebutton.setOnClickListener(click -> {
+            Toast stats = Toast.makeText(
+                    this,
+                    "The width = " + getMeasuredWidth() +
+                    " and height = " + getMeasuredHeight(),
+                    Toast.LENGTH_SHORT);
+            stats.show();
+        })
+
         variableBinding.textview.setText(model.editString);
         variableBinding.mybutton.setOnClickListener(click -> {
             model.editString = variableBinding.my_edit_text.getText().toString();
--- app/src/main/java/algonquin/cst2335/bake0374/data/MainViewModel.java
+++ app/src/main/java/algonquin/cst2335/bake0374/data/MainViewModel.java
@@ -1,8 +1,9 @@
 package algonquin.cst2335.bake0374.data;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel{
-
+    public MutableLiveData<Boolean> isSelected = new MutableLiveData<>();
     public String editString;
 }
