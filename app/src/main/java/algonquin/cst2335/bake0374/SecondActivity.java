e25874c90f7897203c1426d2c8e8eec3a39fa070
morga <morganbakelmun@hotmail.com>
2023-06-08 16:48:02 -0400

    Working on lab 4. I finished part 1, except that the camera does not work. It crashes and returns to the main page... even though there is no listener or command to do that.

--- app/src/main/java/algonquin/cst2335/bake0374/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/bake0374/SecondActivity.java
@@ -1,10 +1,21 @@
 package algonquin.cst2335.bake0374;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
@@ -15,6 +26,32 @@ public class SecondActivity extends AppCompatActivity {
         Intent fromPrevious = getIntent();
         String emailAddress = fromPrevious.getStringExtra("EmailAddress");
         TextView textView2 = findViewById(R.id.textView2);
-        textView2.setText("Welcome Back " + emailAddress);
+        textView2.setText("Welcome back " + emailAddress);
+        ImageView profileImage = findViewById(R.id.profileImage);
+
+        Button changePicture = findViewById(R.id.button2);
+        changePicture.setOnClickListener( clk-> {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            if(checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+                startActivity(cameraIntent);
+            else
+                requestPermissions(new String[] {Manifest.permission.CAMERA}, 20);
+
+            ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>() {
+                        @Override
+                        public void onActivityResult(ActivityResult result) {
+                            if (result.getResultCode() == Activity.RESULT_OK) {
+                                Intent data = result.getData();
+                                Bitmap thumbnail = data.getParcelableExtra("data");
+                                profileImage.setImageBitmap(thumbnail);
+                            }
+                        }
+                    });
+            cameraResult.launch(cameraIntent);
+        });
+
     }
 }
